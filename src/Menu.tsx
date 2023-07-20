5da86c59242c820ebaa6c0ac926a15748b7cebe0
Nicholas <nicholashendrata110900@gmail.com>
2023-07-21 04:20:04 +0700

    Added a new 'menu' page.

---
+++ src/Menu.tsx
@@ -0,0 +1,13 @@
+import { useState, type FC } from "react";
+
+const Menu: FC = () => {
+  const [] = useState();
+  return (
+    <div className="">
+      <h1>Menu Makanan dan Minuman</h1>
+      {/* TODO */}
+    </div>
+  );
+}
+
+export default Menu;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import "./styles.css";
 
 import App from "./App";
 import Dashboard from "./Dashboard";
+import Menu from "./Menu";
 import History from "./History";
 import Report from "./Report";
 
 const routes: Array<RouteObject> = [
   { path: "/", element: <App /> },
   { path: "/dashboard", element: <Dashboard /> },
+  { path: "/menu", element: <Menu /> },
   { path: "/history", element: <History /> },
   { path: "/report", element: <Report /> }
 ];
